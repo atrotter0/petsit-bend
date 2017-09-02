a63e704106a020791c2fcfd8c7adc126c80e182d
Abel Trotter <abel.trotter@getg5.com>
2017-09-01 21:06:16 -0700

    Add users, minor hyperlinklink changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170901052009) do
+ActiveRecord::Schema.define(version: 20170902035426) do
 
   create_table "pets", force: :cascade do |t|
     t.string   "name"
@@ -29,6 +29,13 @@ ActiveRecord::Schema.define(version: 20170901052009) do
     t.datetime "updated_at"
     t.boolean  "medications",          default: false
     t.text     "special_instructions"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
