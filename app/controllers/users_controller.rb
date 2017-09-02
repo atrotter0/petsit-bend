d7badc6e5bc9ecdc4ddbb802b2d0a239cc88ecff
Abel Trotter <abel.trotter@getg5.com>
2017-09-02 01:49:00 -0700

    Style users show and edit views, add columns to users, style updates.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the Alpha Blog, #{@user.username}!"
+      flash[:success] = "Account successfully created! Welcome to the Petsit Bend, #{@user.first_name}!"
       redirect_to user_path(@user)
     else
       render 'new'
@@ -20,6 +20,15 @@ class UsersController < ApplicationController
   end
 
   def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:success] = "Your account was successfully updated!"
+      redirect_to user_path(@user)
+    else
+      render 'edit'
+    end
   end
 
   def show
@@ -31,7 +40,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :email)
+    params.require(:user).permit(:username, :email, :first_name, :last_name, :phone, :address)
   end
 
   def set_user
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170902044431) do
+ActiveRecord::Schema.define(version: 20170902070955) do
 
   create_table "pets", force: :cascade do |t|
     t.string   "name"
@@ -38,6 +38,10 @@ ActiveRecord::Schema.define(version: 20170902044431) do
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "phone"
+    t.text     "address"
   end
 
 end
