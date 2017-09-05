63148b4c8dc494915a73d1ae198b053e867ac26a
Abel Trotter <abel.trotter@getg5.com>
2017-09-04 19:29:26 -0700

    Add pet validations, fix form bug

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,5 +1,9 @@
 class Pet < ActiveRecord::Base
   belongs_to :user
 
-  #validates :user_id, presence: true
+  validates :user_id, presence: true
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :pet_type, presence: true
+  validates :age, presence: true
+  validates :color, presence: true
 end
