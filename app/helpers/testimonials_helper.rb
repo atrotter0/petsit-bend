591a9ab5ebd3c3ab571a324831c19762966111f7
Abel Trotter <a_trotter0@hotmail.com>
2017-10-05 14:09:51 -0700

    Fix spacing

--- app/helpers/testimonials_helper.rb
+++ app/helpers/testimonials_helper.rb
@@ -1,10 +1,10 @@
 module TestimonialsHelper
   def no_approved_testimonials?
-		none_approved = true
-		return if @testimonials.blank?
-		@testimonials.each do |testimonial|
-	  	none_approved = false if testimonial.approved == true
-		end
+    none_approved = true
+    return if @testimonials.blank?
+    @testimonials.each do |testimonial|
+      none_approved = false if testimonial.approved == true
+    end
     none_approved
   end
 end
