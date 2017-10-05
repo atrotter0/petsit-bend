8f6bb61a46c8c656b25a6d2d308aada587b09667
Abel Trotter <a_trotter0@hotmail.com>
2017-10-05 15:28:48 -0700

    Add method to format phone numbers on input

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -5,9 +5,45 @@ function characterCounter(inputFieldId, maxChars, charDisplayDivId) {
   });
 }
 
+function phoneNumberFormatter() {
+  $("#user_phone").keyup(function(evt) {
+    if (keyCodeValid(evt) == true) {
+      var input = formatPhone(this);
+      $("#user_phone").val(input);
+    }
+  });
+}
+
+function keyCodeValid(event) {
+  //check for backspace or delete keys pressed
+  if (event.keyCode == 8 || event.keyCode == 46) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function formatPhone(target) {
+  var input = $(target).val();
+  var length = $(target).val().length
+  input = input.replace(/\D/g,'');
+
+  if (length == 0) {
+    input = input;
+  } else if (length < 4) {
+    input = '(' + input;
+  } else if (length < 6) {
+    input = '(' + input.substring(0,3) + ') ' + input.substring(3,6);
+  } else {
+    input = '('+input.substring(0,3) + ') ' + input.substring(3,6) + '-' + input.substring(6,10);
+  }
+  return input;
+}
+
 $(document).ready(function() {
   characterCounter("#reservation_special_instructions", 250, "#instructions-count");
   characterCounter("#testimonial_body", 800, "#testimonials-count");
+  phoneNumberFormatter();
 
   $('#pet-list').multiSelect({
     selectionHeader: 'Selected',
