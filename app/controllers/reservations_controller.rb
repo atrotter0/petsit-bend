03fef5893280d8a1aecf3dd1554527dc3a53389b
Abel Trotter <a_trotter0@hotmail.com>
2017-10-06 13:28:32 -0700

    Add UserHelper module

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -4,6 +4,7 @@ class ReservationsController < ApplicationController
   include FormHelper
   include ReservationHelper
   include PaginateHelper
+  include UserHelper
   before_action :set_reservation, only: [:edit, :update, :show, :destroy]
   before_action :require_user
   before_action :require_same_user
@@ -68,37 +69,6 @@ class ReservationsController < ApplicationController
 
   def set_reservation
     @reservation = Reservation.find(params[:id])
-  end
-
-  def create_pet_list
-    # we need to clean up pet_list if admin creates the reservation
-    # admin pet_list comes across as: pet_list => ["Frankie, user id: 2", "Orange, user id: 2"]
-    params[:pet_list] = remove_user_ids(params[:pet_list]) if params[:pet_list].first.include?(',')
-    if params[:pet_list].count > 1
-      list = params[:pet_list].map{ |pet| "#{pet}" }.join(", ")
-    else
-      list = params[:pet_list].first
-    end
-    @reservation.pet_list = list
-  end
-
-  def remove_user_ids(pet_list)
-    pet_list.each do |item|
-      pet_list[pet_list.index(item)] = item.split(',').first
-    end
-    pet_list
-  end
-
-  def get_user_id
-    return current_user.id unless params[:user_list].present?
-
-    @reservation.user_id = get_selected_user_id(params[:user_list])
-  end
-
-  def get_selected_user_id(user_list)
-    # we need to get and set the user_id if admin creates the reservation
-    # admin user_list comes across as: user_list = ["Abe Test, user id: 2"]
-    user_list.first.split(':').last.strip!
   end
 
   def require_same_user
---
+++ app/helpers/user_helper.rb
@@ -0,0 +1,50 @@
+module UserHelper
+  def reservaton_user_name(reservation)
+    "#{reservation.user.first_name} #{reservation.user.last_name}"
+  end
+
+  def all_pets_for_users
+    pet_list = []
+    User.all.each do |user|
+      user.pets.each do |pet|
+        pet_list << "#{pet.name}, user id: #{user.id}"
+      end
+    end
+    pet_list
+  end
+
+  def user_name_and_id(user)
+    return "#{user.first_name} #{user.last_name}, user id: #{user.id}"
+  end
+
+  def create_pet_list
+    # we need to clean up pet_list if admin creates the reservation
+    # admin pet_list comes across as: pet_list => ["Frankie, user id: 2", "Orange, user id: 2"]
+    params[:pet_list] = remove_user_ids(params[:pet_list]) if params[:pet_list].first.include?(',')
+    if params[:pet_list].count > 1
+      list = params[:pet_list].map{ |pet| "#{pet}" }.join(", ")
+    else
+      list = params[:pet_list].first
+    end
+    @reservation.pet_list = list
+  end
+
+  def remove_user_ids(pet_list)
+    pet_list.each do |item|
+      pet_list[pet_list.index(item)] = item.split(',').first
+    end
+    pet_list
+  end
+
+  def get_selected_user_id(user_list)
+    # we need to get and set the user_id if admin creates the reservation
+    # admin user_list comes across as: user_list = ["Abe Test, user id: 2"]
+    user_list.first.split(':').last.strip!
+  end
+
+  def get_user_id
+    return current_user.id unless params[:user_list].present?
+
+    @reservation.user_id = get_selected_user_id(params[:user_list])
+  end
+end
