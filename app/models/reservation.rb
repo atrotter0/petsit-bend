1255c653e75619874cd95c73958311239f3ca676
Abel Trotter <a_trotter0@hotmail.com>
2017-10-18 19:08:14 -0700

    Add end_date validations to reservation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -6,4 +6,18 @@ class Reservation < ActiveRecord::Base
   validates :end_date, presence: true
   validates :special_instructions, length: { maximum: 250 }
   validates :pet_list, presence: true
+
+  validate :check_end_date
+
+  def check_end_date
+    unless valid_end_date?
+      self.errors.add(:reservation_end_date, "can't be before start date")
+    end
+  end
+
+  def valid_end_date?
+    end_date = Date.strptime(self.end_date, '%m/%d/%Y').to_date
+    start_date = Date.strptime(self.start_date, '%m/%d/%Y').to_date
+    end_date >= start_date
+  end
 end
