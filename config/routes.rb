ed7dbae00a16afb658900dbdcf3e821aeae1a278
Abel Trotter <a_trotter0@hotmail.com>
2017-11-23 12:50:41 -0800

    Add contact form controller, view, routes, styles

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   resources :testimonials
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :account_activations, only: [:new, :create, :edit]
+  resources :contact_forms, only: [:new, :create]
 
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
