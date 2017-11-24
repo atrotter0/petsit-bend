299a356fe99fbac65173ca6eabd11fdb1e07b094
Abel Trotter <a_trotter0@hotmail.com>
2017-11-24 15:40:47 -0800

    Add name and timestamp to Testimonial, adjust icon

--- app/models/testimonial.rb
+++ app/models/testimonial.rb
@@ -2,4 +2,8 @@ class Testimonial < ActiveRecord::Base
   belongs_to :user
   
   validates :body, presence: true, length: { maximum: 800 }
+
+  def display_short_date
+    self.created_at.strftime('%b %Y')
+  end
 end
