527b28ed3b8c9f1b04b503b8bd279dd9742eb9b0
Abel Trotter <a_trotter0@hotmail.com>
2017-11-28 16:02:30 -0800

    Add btn toggles and time displays, styles

--- app/assets/javascripts/walking_schedule.js
+++ app/assets/javascripts/walking_schedule.js
@@ -17,38 +17,61 @@ function getTimeFromSelect(day, section) {
   var periodId = '#' + day + '-' + section + '-period-select-box';
   var timeSelect = $(timeId).children('select');
   var periodSelect = $(periodId).children('select');
-  var finalTimeVal = timeSelect[0].value + ' ' + periodSelect[0].value;
-  console.log(finalTimeVal);
-  return finalTimeVal;
+  var formattedTimeVal = timeSelect[0].value + ' ' + periodSelect[0].value;
+  console.log(formattedTimeVal);
+  return formattedTimeVal;
 }
 
-function showTimeBox(day) {
+function showTimeSelection(day) {
   var timeBoxId = '#' + day + '-time-select-box';
   console.log('toggling: ' + timeBoxId);
   $(timeBoxId).toggle('fast');
 }
 
-function addToSchedule(day, time, object) {
-  if (object.hasClass('add-time-clicked')) {
-    console.log('do smething');
+function addTime(day, time, section, btnObject) {
+  var check_one = $(btnObject).hasClass('add-time-clicked');
+  var timeItemId = '#' + day + '-visit-' + section + '-schedule';
+  if (check_one) {
+    $(timeItemId).text(time).toggle('fast');
   } else {
-    console.log('do something else');
+    $(timeItemId).toggle('fast');
   }
+}
+
+function disableTimeSelect(day, section) {
+  var timeId = '#' + day + '-' + section + '-time-select-box';
+  var periodId = '#' + day + '-' + section + '-period-select-box';
+  if ($(timeId).children('select').hasClass('disabled')) {
+    $(timeId).children('select').removeClass('disabled').removeAttr('disabled');
+    $(periodId).children('select').removeClass('disabled').removeAttr('disabled');
+  } else {
+    $(timeId).children('select').addClass('disabled').attr('disabled', 'disabled');
+    $(periodId).children('select').addClass('disabled').attr('disabled', 'disabled');
+  }
+}
+
+function toggleBtnGlyphs(btnObject) {
+  $(btnObject).toggleClass('btn-primary').toggleClass('add-time-clicked');
+  $(btnObject).children('span').toggleClass('glyphicon glyphicon-plus-sign').toggleClass('glyphicon glyphicon-remove');
 }
 
 $(document).ready(function() {
   $('#sunday-button, #monday-button').click(function() {
-    showTimeBox(getPrefix(this.id));
+    showTimeSelection(getPrefix(this.id));
     $(this).toggleClass('btn-toggle-clicked');
     $(this).children('span').toggleClass('glyphicon glyphicon-plus').toggleClass('glyphicon glyphicon-minus');
   });
 
-  $('#sunday-visit-1-btn, #sunday-visit-2-btn').click(function() {
+  $('#sunday-visit-1-btn, #sunday-visit-2-btn, #sunday-visit-3-btn').click(function() {
     var day = getPrefix(this.id);
     var section = getSection(this.id);
     var time = getTimeFromSelect(day, section);
-    addToSchedule(day, time, this);
-    $(this).toggleClass('btn-primary').toggleClass('add-time-clicked');
-    $(this).children('span').toggleClass('glyphicon glyphicon-ok').toggleClass('glyphicon glyphicon-remove');
+    toggleBtnGlyphs(this);
+    disableTimeSelect(day, section);
+    addTime(day, time, section, this);
+  });
+
+  $('#0-btn-add-pet, #1-btn-add-pet, #2-btn-add-pet, #3-btn-add-pet, #4-btn-add-pet, #5-btn-add-pet, #6-btn-add-pet, #7-btn-add-pet, #8btn-add-pet, #9-btn-add-pet').click(function() {
+    toggleBtnGlyphs(this);
   });
 });
