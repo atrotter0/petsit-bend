0fdf57858d188aeac014c8d88e4884411afdbd58
Abel Trotter <a_trotter0@hotmail.com>
2018-03-11 17:32:24 -0700

    Wire up awesomplete, create ajax calls for Dog API

--- app/assets/javascripts/breeds.js
+++ app/assets/javascripts/breeds.js
@@ -5,13 +5,37 @@ function hideBreedGroup() {
 function checkPetType(pet) {
   if (pet == "Dog") {
     $('#pet-breed-group').css('display', 'block');
+    getPetData();
   } else {
     hideBreedGroup();
   }
 }
 
+function getPetData() {
+  $.ajax({
+    url: "https://dog.ceo/api/breeds/list",
+    success: function(data) {
+      var dogList = data.message;
+      addDogs(dogList);
+    },
+    error: function(err) {
+      console.log(err);
+    }
+  })
+}
+
+function addDogs(list) {
+  for(var i = 0; i < list.length; i++) {
+    $('#dogDataList').append('<option>' + list[i].capitalize() + '</option>');
+  }
+}
+
+String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+}
+
 $(document).ready(function() {
-  hideBreedGroup();
+  checkPetType($('#pet_pet_type').val());
 
   $('#pet_pet_type').change(function() {
     var selected = $(this).val();
